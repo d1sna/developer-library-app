a40d246c54b77e1c230cc10c762c451e7b7f0638
Aleksandr Dis <a.dis@awatera.com>
2022-04-04 20:30:57 +0400

    pure next project

---
+++ pages/components/Title.jsx
@@ -0,0 +1,10 @@
+import Head from "next/head";
+
+export default function Title(props) {
+  return (
+    <Head>
+      <title>DLA | Welcome</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+}
